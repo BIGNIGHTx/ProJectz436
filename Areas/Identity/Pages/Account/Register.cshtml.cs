99ece9cc22caab80b92819548ea7255a5a0a2519
TunyatapVich <tunyatap.vich@bumail.net>
2024-11-12 22:03:58 +0700

    Input box Fix

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,19 @@ namespace FinalProject.Areas.Identity.Pages.Account
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(255, ErrorMessage = "Please Select Department", MinimumLength = 1)]
+            public string Department { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "Please Confirm Password", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Email { get; set; }
         }
 
 
